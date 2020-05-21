dc79252438157c8dfc9060fd766c045845d3c5bc
Damian Wędzikowski <dwedzikowski@gmail.com>
2020-05-21 20:13:56 +0200

    Add files via upload

---
+++ lab9/Controllers/SongsController_old.cs
@@ -0,0 +1,39 @@
+﻿using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.Controllers
+{
+    public class SongsController_old : Controller
+    {
+
+        // GET: Songs
+        public ActionResult Index()
+        {
+            Song song = new Song();
+            song.Id = 1;
+            song.Name = "Utwór 1";
+            song.Artist = "Wykonawca 1";
+            song.GenreId = 1;
+            return View(song);
+        }
+
+        public ActionResult Square(int? id)
+        {
+            if (id == null)
+            {
+                return Content("Brak parametru");
+            }
+            else
+            {
+                return Content((id * id).ToString());
+            }
+        }
+
+    }
+}
---
+++ lab9/Models/Genre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Genre name is required!")]
+        [StringLength(100, ErrorMessage = "Maximal length of the name of a genre is 100 characters!")]
+        public string Name { get; set; }
+        public ICollection<Song> SongsList { get; set; }
+    }
+}
